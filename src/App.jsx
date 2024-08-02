400a30b539fa9e01f515cf28c3183835b2798fb2
Zain Waseem <zainwaseem9371@gmail.com>
2024-08-02 22:36:37 +0500

    feature changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,12 +9,8 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
       </div>
       <h1>React CI CD Done</h1>
       <div className="card">
